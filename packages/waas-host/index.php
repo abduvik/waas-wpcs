7775ea5c175ff40a403186a8233ed4df1722d90a
Wijnand <wvleeuwen@gmail.com>
2023-12-04 19:05:12 -0800

    Bump version to 2.2.4

--- packages/waas-host/index.php
+++ packages/waas-host/index.php
@@ -26,14 +26,14 @@ require_once 'vendor/autoload.php';
 
 /**
  * @package WaaSHost
- * @version 2.2.3
+ * @version 2.2.4
  */
 /*
 Plugin Name: WaaS Host
 Plugin URI: https://github.com/Daxez/waas-wpcs
 Description: This plugin is used to create tenants on WPCS.io with support of WordPress, WooCommerce, WooCommerce Subscriptions and Self-service Dashboard for WooCommerce Subscriptions.
 Author: WPCS
-Version: 2.2.3
+Version: 2.2.4
 Author URI: https://wpcs.io
 Update URI: wpcs-waas-host
 */
@@ -41,7 +41,7 @@ Update URI: wpcs-waas-host
 define( 'WPCS_WAAS_HOST_SLUG', 'wpcs-waas-host' );
 define( 'WPCS_WAAS_HOST_BASENAME', plugin_basename( __FILE__ ) );
 define( 'WPCS_WAAS_HOST_UPDATE_URI', 'wpcs-waas-host' );
-define( 'WPCS_WAAS_HOST_VERSION', '2.2.3' );
+define( 'WPCS_WAAS_HOST_VERSION', '2.2.4' );
 define( 'WPCS_WAAS_HOST_TEXTDOMAIN', 'wpcs-waas-host-textdomain' );
 
 // Controllers to list for APIs
